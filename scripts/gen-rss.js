60d0f9b2a70388213600b5a08b339b1993cd8268
qujones1 <32285137+qujones1@users.noreply.github.com>
2022-04-14 13:26:43 -0400

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -10,20 +10,20 @@ async function generate() {
     feed_url: 'https://yoursite.com/feed.xml'
   })
 
-  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'projects'))
+  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
 
   await Promise.all(
     posts.map(async (name) => {
       if (name.startsWith('index.')) return
 
       const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'projects', name)
+        path.join(__dirname, '..', 'pages', 'posts', name)
       )
       const frontmatter = matter(content)
 
       feed.item({
         title: frontmatter.data.title,
-        url: '/projects/' + name.replace(/\.mdx?/, ''),
+        url: '/posts/' + name.replace(/\.mdx?/, ''),
         date: frontmatter.data.date,
         description: frontmatter.data.description,
         categories: frontmatter.data.tag.split(', '),
